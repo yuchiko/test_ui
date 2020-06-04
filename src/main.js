12ec82bbd35112fee45dc98700114a276881f534
yuchiko <iyuchiko@gmail.com>
2020-06-05 00:46:30 +0300

    tested locale library

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HkInterface from 'hk-interface';
+import 'hk-interface/dist/hkInterface.css';
+
+
+Vue.use(HkInterface);
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
